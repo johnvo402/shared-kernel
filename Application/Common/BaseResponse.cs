096decce48feb0b03134fa05f039ca6bacb6172b
John Vo <jvanime212@gmail.com>
2025-05-20 20:29:12 +0700

    fix response api

--- Application/Common/BaseResponse.cs
+++ Application/Common/BaseResponse.cs
@@ -13,7 +13,24 @@ public class BaseResponse : DefaultBaseResponse, IBaseAuditable
 
 public class DefaultBaseResponse
 {
-    public Ulid Id { get; set; }
+    public long Id { get; set; }
+    public Ulid PublicId { get; set; }
+
+    public DateTimeOffset CreatedAt { get; set; }
+}
+
+public class BaseResponse<T> : DefaultBaseResponse<T>, IBaseAuditable
+{
+    public string CreatedBy { get; set; } = string.Empty;
+
+    public string? UpdatedBy { get; set; }
+
+    public DateTimeOffset? UpdatedAt { get; set; }
+}
+
+public class DefaultBaseResponse<T>
+{
+    public T Id { get; set; } = default!;
 
     public DateTimeOffset CreatedAt { get; set; }
 }
